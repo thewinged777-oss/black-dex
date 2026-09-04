95f4bc9076df7f053c3e95f6971f10a84f7855e5
thewinged777-oss <thewinged777@gmail.com>
2026-09-04 15:32:55 +0300

    Add News desk on main landing
    * Add News desk: homepage cards, /news page, footer link
    
    * Add news page copy and news card styles
    
    * Add /news.html with MoonPay and Morpho Earn posts
    
    * Replace X feed slot with News cards on main landing

--- host-router.js
+++ host-router.js
@@ -4,10 +4,14 @@
   var map = {
     "listing.black-dex.online": "/listing.html",
     "affiliates.black-dex.online": "/affiliates.html",
-    "token.black-dex.online": "/token.html"
+    "token.black-dex.online": "/token.html",
+    "news.black-dex.online": "/news.html"
   };
   var dest = map[host];
   if (dest && path !== dest && path !== dest.replace(".html", "")) {
     location.replace(dest + location.search + location.hash);
+  }
+  if (path === "/news" || path === "/news/") {
+    location.replace("/news.html" + location.search + location.hash);
   }
 })();
